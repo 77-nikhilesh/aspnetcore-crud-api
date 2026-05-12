526b5d919469b0240454653eef478a734fc3959c
Nikhilesh <nikhileshyadavkk@gmail.com>
2026-05-12 10:54:39 +0530

    Clean project files

---
+++ EmployeeAdminPortal/EmployeeAdminPortal.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeAdminPortal.Api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Api.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeAdminPortal/EmployeeAdminPortal.Api/README.md
@@ -0,0 +1,24 @@
+﻿# ASP.NET Core Web API CRUD Project
+
+## Features
+- CRUD Operations
+- Entity Framework Core
+- SQL Server
+- REST API
+- Swagger Integration
+
+## Technologies
+- ASP.NET Core
+- C#
+- SQL Server
+- Entity Framework Core
+
+## Setup
+1. Clone repository
+2. Add your connection string
+3. Run migrations
+4. Start project
+
+## Run
+```bash
+dotnet run
