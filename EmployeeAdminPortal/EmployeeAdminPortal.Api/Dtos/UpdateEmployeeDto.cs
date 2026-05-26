762bd8c9ce0c2c43ad90364f7f51b6b13d8b1567
Nikhilesh <nikhileshyadavkk@gmail.com>
2026-05-26 11:17:01 +0530

    Added validation attributes to Dtos

--- EmployeeAdminPortal/EmployeeAdminPortal.Api/Dtos/UpdateEmployeeDto.cs
+++ EmployeeAdminPortal/EmployeeAdminPortal.Api/Dtos/UpdateEmployeeDto.cs
@@ -1,10 +1,23 @@
-﻿namespace EmployeeAdminPortal.Api.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeAdminPortal.Api.Dtos
 {
     public class UpdateEmployeeDto
     {
-        public required string Name { get; set; }
-        public required string Email { get; set; }
-        public string? Phone { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required] 
+        [StringLength(50)]
         public decimal Salary { get; set; }
     }
 }
